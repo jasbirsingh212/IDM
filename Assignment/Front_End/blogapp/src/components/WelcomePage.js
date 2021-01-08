1a431eac7cbaa7bb98fc7c90c5b19749eb0aaef5
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-01-08 18:50:27 +0530

    Assignment done

--- Assignment/Front_End/blogapp/src/components/WelcomePage.js
+++ Assignment/Front_End/blogapp/src/components/WelcomePage.js
@@ -3,17 +3,31 @@ import logo from "./../resources/logo512.png"
 
 function WelcomePage(props)
 {
-   
-
+   if(props.location && props.location.state && props.location.state.userEmail)
+   {
     return (
         <div id='home'>
-            <h1 className="head">Welcome Page</h1>
+            <h1 className="head">Welcome {props.location.state.userEmail}</h1>
             <div className='container'>
             <img src={logo} alt="home">
             </img>
             </div>
         </div>
     )
+   }
+   else{
+    return (
+        <div id='home'>
+            <h1 className="head">Please SignIn to Get Started</h1>
+            <div className='container'>
+            <img src={logo} alt="home">
+            </img>
+            </div>
+        </div>
+    )
+    
+
+   }
 }
 
 export default WelcomePage;
